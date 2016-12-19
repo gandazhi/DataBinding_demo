e53e3fc10e6fa479a0d40f83f2fcc59b25ec121b
gandazhi <18228438700@163.com>
2016-12-19 21:59:26 +0800

    1.学会了DataBinding生成类的命名规则 2.xml里对控件进行绑定

--- app/src/main/java/com/gandazhi/databinding_demo/MainActivity.java
+++ app/src/main/java/com/gandazhi/databinding_demo/MainActivity.java
@@ -1,13 +1,35 @@
 package com.gandazhi.databinding_demo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gandazhi.databinding_demo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    User user = new User("gandazhi");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /**
+         * DataBinding类生成的规则
+         * 如下xml文件为activity_main
+         * 下划线前的首字母大写，下划线之后的首字母大写
+         * 所有生成的类都会有Binding后缀
+         */
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setUser(user);
+        //binding.setVariable(BR.user,user);
+        binding.setPresenter(new Presenter());
+    }
+
+    public class Presenter {
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "onClick", Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/gandazhi/databinding_demo/User.java
@@ -0,0 +1,21 @@
+package com.gandazhi.databinding_demo;
+
+/**
+ * Created by gandazhiPc on 2016/12/19.
+ */
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
